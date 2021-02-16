2ebb96b70e0f9983fb653c9a377c8c10660c02f8
Eduardo Azevedo Grigolo <eduardo.grigolo@frontiercargroup.com>
2021-02-16 16:47:24 +0100

    Update setup script file
    Check the destination folder for eslint, prettier and vscode config files.
    If it doesn't have it, create them! Or the ones missing.

---
+++ CHANGELOG.md
@@ -0,0 +1,49 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+### To Add
+
+### To Change
+
+### To Remove
+
+### To Fix
+
+## [0.1.0] - 2021-02-16
+
+### Added
+
+- Refactored setup script to add `.eslintrc.js`, `.prettierrc.js` and `.vscode/settings.json` files for better experience
+
+## [0.0.2] - 2021-02-16
+
+### Changed
+
+- Updated @typescript-eslint/parser to 4.15.1
+- Updated eslint to 7.20.0
+- Updated eslint-config-prettier to 7.2.0
+- Updated eslint-plugin-prettier to 3.3.1
+
+## [0.0.1] - 2020-11-29
+
+### Added
+
+- Eslint config based on financing-app repo
+
+## [0.0.0] - 2020-11-29
+
+### Added
+
+- Just an initial commit for the repository.
+
+[unreleased]: https://git.frontiercargroup.org/tools/eslint-config/-/compare/0.1.0...master
+[0.1.0]: https://git.frontiercargroup.org/tools/eslint-config/-/tags/0.1.0
+[0.0.2]: https://git.frontiercargroup.org/tools/eslint-config/-/tags/0.0.2
+[0.0.1]: https://git.frontiercargroup.org/tools/eslint-config/-/tags/0.0.1
+[0.0.0]: https://git.frontiercargroup.org/tools/eslint-config/-/tags/0.0.0
--- utils/setup.js
+++ utils/setup.js
@@ -4,10 +4,11 @@ const fs = require('fs')
 const execa = require('execa')
 const rootDir = require('./root')
 const ora = require('ora');
+const globby = require('globby');
 
-async function start() {
+async function installPeerDependencies(spinner) {
   const fileName = `${rootDir}/package.json`
-  const spinner = ora('Installing peer dependencies').start();
+  spinner.text = 'Installing peer dependencies'
 
   // Check if file exists
   if (fs.existsSync(fileName)) {
@@ -23,16 +24,87 @@ async function start() {
     for (const [key, value] of Object.entries(peerDepObject)) {
       peerDepArray.push(`${key}@${value}`)
     }
-    // Then run `npm i --save-dev [all pacakges@versions]`
+
+    // Then run `npm i --save-dev [all packages@versions]`
     try {
       const {stdout} = await execa('npm', ['i', '--save-dev', ...peerDepArray])
-      spinner.succeed(`Peer dependencies installed successfully!\n\n${stdout}`)
+      spinner.text = `Peer dependencies installed successfully!\n\n${stdout}`
     } catch (error) {
       spinner.fail(`Peer dependencies weren't installed!\n\n${error}`)
     }
   } else {
     // Throw error if file is missing
     spinner.fail(`File ${fileName} not found!`)
+  }
+}
+
+async function createEslintConfig(spinner) {
+  spinner.text = 'Checking for eslint config file'
+
+  const foundFile = await globby(['.eslint*', '!.eslintignore']);
+
+  // Check if file exists
+  if (foundFile.length > 0) {
+    // If there's a file already, don't do anything
+    spinner.text = 'Eslint config file already exists!'
+  } else {
+    fs.copyFileSync(`${rootDir}/utils/templates/.eslintrc.js`, './.eslintrc.js', () => {
+      spinner.text = 'Eslint config file created!'
+    })
+  }
+}
+
+async function createPrettierConfig(spinner) {
+  spinner.text = 'Checking for eslint config file'
+
+  const foundFile = await globby(['.prettierrc.*', 'prettier.config.*']);
+
+  // Check if file exists
+  if (foundFile.length > 0) {
+    // If there's a file already, don't do anything
+    spinner.text = 'Prettier config file already exists!'
+  } else {
+    fs.copyFileSync(`${rootDir}/utils/templates/.prettierrc.js`, './.prettierrc.js', () => {
+      spinner.text = 'Prettier config file created!'
+    })
+  }
+}
+
+async function createVscodeSettings(spinner) {
+  spinner.text = 'Checking for eslint config file'
+
+  const foundFile = await globby(['.vscode/settings.json']);
+
+  // Check if file exists
+  if (foundFile.length > 0) {
+    // If there's a file already, don't do anything
+    spinner.text = 'Eslint config file already exists!'
+  } else {
+    if (!fs.existsSync('./.vscode')) {
+      fs.mkdirSync('./.vscode')
+    }
+
+    fs.copyFileSync(`${rootDir}/utils/templates/settings.json`, './.vscode/settings.json', () => {
+      spinner.text = 'Eslint config file created!'
+    })
+  }
+}
+
+async function start() {
+  const spinner = ora('Starting configuration scripts...').start();
+
+  try {
+    // Install peerDependencies
+    // await installPeerDependencies(spinner)
+    // Check if .eslintrc.* file exists
+    await createEslintConfig(spinner)
+    // Check if .prettierrc.* or prettier.config.* file exists
+    await createPrettierConfig(spinner)
+    // Check if a folder .vscode with a settings.json file exists
+    await createVscodeSettings(spinner)
+    spinner.succeed(`Repository configuration finished.`)
+  } catch (error) {
+    spinner.fail(`Repository wasn't configured because of an issue...\n\n${error}`)
   }
 }
 
