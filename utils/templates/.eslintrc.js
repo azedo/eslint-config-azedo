c389442d636b7a758c7675125de40ce2e243ba35
Eduardo Azevedo Grigolo <eduardo.grigolo@frontiercargroup.com>
2021-02-16 16:40:31 +0100

    Add logic to create eslint, prettier and vscode settings files
    Update the setup script to check the destination folder (where you are running the setup script) for eslint, prettier and vscode settings/config files.

---
+++ utils/templates/.eslintrc.js
@@ -0,0 +1,5 @@
+// NOTE: This is a basic functional implementation. Please adjust it accordingly!
+
+module.exports = {
+  extends: ['@fcg/eslint-config'],
+}
---
+++ utils/templates/.prettierrc.js
@@ -0,0 +1,7 @@
+// NOTE: This is a basic functional implementation. Please adjust it accordingly!
+
+module.exports = {
+  printWidth: 120,
+  singleQuote: true,
+  semi: false,
+}
